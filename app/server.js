542c9b3e98031192ba997a869db3921f096e9cf6
Raylan Campos <raylan@raylansoares.com>
2020-04-20 19:09:00 -0300

    Add io test events

--- app/server.js
+++ app/server.js
@@ -10,10 +10,20 @@ const client = configs.client;
 
 client.connect()
 
+// client.on("chat", async (channel, user, message, self) => {
+    // socket.emit('client-chat', message) // send message to rose-server
+// });
+
+// socket.on('server-confirm', function (data) { // get confirmation from rose-server
+    // client.action(channel, `confirmed message: ${data}`) // send confirmation message on twitch chat
+// });
+
 client.on("chat", async (channel, user, message, self) => {
-    socket.emit('client-chat', message) // send message to rose-server
+    if (message === 'sub') {
+        socket.emit('requestPrize', user['display-name']) // send message to rose-server
+    }
 });
 
-socket.on('server-confirm', function (data) { // get confirmation from rose-server
-    client.action(channel, `confirmed message: ${data}`) // send confirmation message on twitch chat
+socket.on('confirmPrize', function (data) { // get confirmation from rose-server
+    client.say(channel, `${data.user} gets ${data.prize}!`) // send confirmation message on twitch chat
 });

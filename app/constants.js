bed34cceaa123669efd0746bea0f695676a538a4
Raylan Campos <raylan@raylansoares.com>
2020-04-20 19:09:17 -0300

    Add io test events

--- app/constants.js
+++
@@ -1,7 +0,0 @@
-const SERVER_HOST = 'http://localhost'
-const SERVER_PORT = 8000
-
-export {
-	SERVER_HOST,
-	SERVER_PORT
-}
--- app/server.js
+++ app/server.js
@@ -1,29 +1,20 @@
 import io from 'socket.io-client';
 
-import * as constants from './constants';
 import * as configs from './configs';
 
-const socket = io(`${constants.SERVER_HOST}:${constants.SERVER_PORT}`);
+const socket = io(`${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
 
 const channel = `#${configs.channel}`;
 const client = configs.client;
 
 client.connect()
 
-// client.on("chat", async (channel, user, message, self) => {
-    // socket.emit('client-chat', message) // send message to rose-server
-// });
-
-// socket.on('server-confirm', function (data) { // get confirmation from rose-server
-    // client.action(channel, `confirmed message: ${data}`) // send confirmation message on twitch chat
-// });
-
 client.on("chat", async (channel, user, message, self) => {
     if (message === 'sub') {
-        socket.emit('requestPrize', user['display-name']) // send message to rose-server
+        socket.emit('requestPrize', user['display-name'])
     }
 });
 
-socket.on('confirmPrize', function (data) { // get confirmation from rose-server
-    client.say(channel, `${data.user} gets ${data.prize}!`) // send confirmation message on twitch chat
+socket.on('confirmPrize', function (data) {
+    client.action(channel, `${data.user} gets ${data.prize}!`)
 });

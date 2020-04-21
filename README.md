65d1aa0b8c9d07b59ee63557d124ed6d81efa3fe
Raylan Campos <raylan@raylansoares.com>
2020-04-21 16:05:36 -0300

    Update readme and require dotenv

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ Open .env file and put you configs
 * CHANNEL - The channel that the bot should connect
 * USERNAME - The bot username
 * PASSWORD - The bot oauth password (Generated on https://twitchapps.com/tmi/)
+* SERVER_HOST - The rose-server host
+* SERVER_PORT - The rose-server port
 
 Start chatbot by typing
 ```sh
--- app/server.js
+++ app/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 import io from 'socket.io-client';
 
 import * as configs from './configs';
@@ -9,12 +11,16 @@ const client = configs.client;
 
 client.connect()
 
+// Event from Twitch chat
 client.on("chat", async (channel, user, message, self) => {
     if (message === 'sub') {
+        // Event to rose-server
         socket.emit('requestPrize', user['display-name'])
     }
 });
 
+// Event from rose-server
 socket.on('confirmPrize', function (data) {
+    // Event to Twitch chat
     client.action(channel, `${data.user} gets ${data.prize}!`)
 });

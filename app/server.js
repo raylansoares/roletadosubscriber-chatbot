3bd749cc9e8481fa35d41c0ae6f23b5de895017f
Raylan Campos <raylan@raylansoares.com>
2020-06-15 21:20:31 -0300

    Add simple try/catch blocks

--- app/server.js
+++ app/server.js
@@ -42,13 +42,16 @@ socket.on('newChannel', function () {
 
 // Event from Twitch chat
 client.on("chat", async (channel, user, message, self) => {
-    if (message === 'TestSubWheel' && user.username === channel.replace('#', '')) {
-        setTimeout(() => {
-            const channelObject = channels.find(findChannel => findChannel.channel === channel)
-            if (!channelObject) return
-            socket.emit('requestPrize', { code: channelObject.code, username: user['display-name']});
-        }, 1000)
-    }
+    try {
+        if (message === 'TestSubWheel' && user.username === channel.replace('#', '')) {
+            setTimeout(() => {
+                const channelObject = channels.find(findChannel => findChannel.channel === channel)
+                if (!channelObject) return
+                socket.emit('requestPrize', { code: channelObject.code, username: user['display-name']});
+            }, 1000)
+        }
+    } catch (e) {}
+    
 });
 
 const replace = (string, data) => {
@@ -57,48 +60,55 @@ const replace = (string, data) => {
 
 // Event from rose-server
 socket.on('confirmPrize', function (data) {
+    try {
+        const channelObject = channels.find(findChannel => findChannel.code === data.code)
+        if (!channelObject) return
 
-    const channelObject = channels.find(findChannel => findChannel.code === data.code)
-    if (!channelObject) return
-
-    // Event to Twitch chat
-    setTimeout(() => {
-        client.action(channelObject.channel, replace(data.prizes[data.prizes.length - 1].message, data))
-    }, 5000)
-
-    const command = data.prizes[data.prizes.length - 1].command
-
-    if(command) {
-        if (command[0] === '@') return
-        const delay = 6000 + ((data.prizes[data.prizes.length - 1].delay || 0) * 1000)
+        // Event to Twitch chat
         setTimeout(() => {
-            client.say(channelObject.channel, replace(command, data))
-        }, delay)
-    }
+            client.action(channelObject.channel, replace(data.prizes[data.prizes.length - 1].message, data))
+        }, 5000)
+
+        const command = data.prizes[data.prizes.length - 1].command
+
+        if(command) {
+            if (command[0] === '@') return
+            const delay = 6000 + ((data.prizes[data.prizes.length - 1].delay || 0) * 1000)
+            setTimeout(() => {
+                client.say(channelObject.channel, replace(command, data))
+            }, delay)
+        }
+    } catch (e) {}
 });
 
 /* SUB EVENTS */
 
 client.on("subscription", function (channel, username, methods, msg, userstate) {
-    setTimeout(() => {
-        const channelObject = channels.find(findChannel => findChannel.channel === channel)
-        if (!channelObject) return
-        socket.emit('requestPrize', { code: channelObject.code, username: username});
-    }, 10000)
+    try {
+        setTimeout(() => {
+            const channelObject = channels.find(findChannel => findChannel.channel === channel)
+            if (!channelObject) return
+            socket.emit('requestPrize', { code: channelObject.code, username: username});
+        }, 10000)
+    } catch (e) {}
 });
  
 client.on("resub", function (channel, username, streakMonths, msg, userstate, methods) {
-    setTimeout(() => {
-        const channelObject = channels.find(findChannel => findChannel.channel === channel)
-        if (!channelObject) return
-        socket.emit('requestPrize', { code: channelObject.code, username: username});
-    }, 10000)
+    try {
+        setTimeout(() => {
+            const channelObject = channels.find(findChannel => findChannel.channel === channel)
+            if (!channelObject) return
+            socket.emit('requestPrize', { code: channelObject.code, username: username});
+        }, 10000)
+    } catch (e) {}
 });
   
 client.on("subgift", function (channel, username, streakMonths, recipient, methods, userstate) {
-    setTimeout(() => {
-        const channelObject = channels.find(findChannel => findChannel.channel === channel)
-        if (!channelObject) return
-        socket.emit('requestPrize', { code: channelObject.code, username: recipient});
-    }, 10000)
+    try {
+        setTimeout(() => {
+            const channelObject = channels.find(findChannel => findChannel.channel === channel)
+            if (!channelObject) return
+            socket.emit('requestPrize', { code: channelObject.code, username: recipient});
+        }, 10000)
+    } catch (e) {}
 });
